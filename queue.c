7eef4701081d3ca97928f17f6db1af78df0f8346
giladgafny1 <giladgafny@campus.technion.ac.il>
2022-06-09 12:03:55 +0000

    started with some files

--- queue.c
+++ queue.c
@@ -74,5 +74,6 @@ int dequeue(Queue* queue, QueueResult* error_code)
     else
         queue->head = NULL;
     *error_code = QUEUESUCCESS;
+    queue->size--;
     return ret_value;
 }
--- thread_pool.h
+++ thread_pool.h
@@ -4,11 +4,16 @@
 
 #ifndef HW3_OS_MYWORK_THREAD_POOL_H
 #define HW3_OS_MYWORK_THREAD_POOL_H
-
+#include "queue.h"
+#include <pthread.h>
 
 typedef struct tpool_t
 {
-    Queue
+    pthread_mutex_t requests_m;
+    pthread_cond_t request_avail;
+    pthread_cond_t
+    Queue aviable_threads;
+
 }Tpool;
 
 Tpool* CreateTpool(int num_of_threads);
